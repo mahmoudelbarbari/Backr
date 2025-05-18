acd6b0a77420d0a4dbada8c5514985b18c611d4d
ahmed-mohamed33 <a7medselim93@gmail.com>
2025-05-18 19:15:57 +0300

    fixed title

--- js/unauthorized.js
+++ js/unauthorized.js
@@ -1,5 +1,6 @@
 const user = JSON.parse(sessionStorage.getItem("user"));
 if (user) {
+    document.title = "Unauthorized Access";
   document.querySelector(".unauthorized-title").innerHTML = "Unauthorized Access";
 document.querySelector(".unauthorized-message").innerHTML = "You are not authorized to access this page. Please contact the administrator if you believe this is an error.";
 document.querySelector(".Action-btn").innerHTML = "Go to Home";
