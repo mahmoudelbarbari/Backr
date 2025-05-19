2adf4b100ea4f99228e1e81cf66f681959950bd4
ahmed-mohamed33 <a7medselim93@gmail.com>
2025-05-19 13:25:07 +0300

    Update user profile functionality; change user details in db.json, enhance profile.js for email and name updates

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,4 @@
+import { User } from '../js/apiCalls.js';
 function addScrolledClass() {
     const navbar = document.querySelector('.backr-navbar');
     if (navbar && window.scrollY > document.querySelector('.settings-header').offsetHeight) {
@@ -58,3 +59,54 @@ window.addEventListener('scroll', addScrolledClass);
 
 
 window.addEventListener('DOMContentLoaded', checkUser);
+async function updateEmailFname() {
+    let user = JSON.parse(localStorage.getItem('user'));
+    const profile = document.getElementById('profile');
+    const userFname = document.getElementById('userFName');
+    const userEmail = document.getElementById('userEmail');
+    const updateBtn = document.getElementById('updateBtn');
+const errorMessage = document.createElement('p');
+    const successMessage = document.createElement('p');
+    errorMessage.className = 'error-message';
+    successMessage.className = 'success-message';
+
+    updateBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+
+
+        if (userFname.value === '' || userEmail.value === '') {
+            errorMessage.textContent = 'Please fill in all fields';
+            return;
+        }
+        const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (regexEmail.test(userEmail.value) === false) {
+            errorMessage.textContent = 'Invalid email address';
+            return;
+        }
+        if (userFname.value.length < 3) {
+            errorMessage.textContent = 'Name must be at least 3 characters long';
+            return;
+        }
+        if (userFname.value === user.name && userEmail.value === user.email) {
+            errorMessage.textContent = 'No changes made';
+            return;
+        }
+
+        try {
+          
+await User.updateUser(user.id, {
+            name: userFname.value,
+            email: userEmail.value
+        });
+user = await User.getUsersById(user.id);
+           localStorage.removeItem('user');
+localStorage.setItem("user", JSON.stringify(user));
+            successMessage.textContent = 'Profile updated successfully';
+        } catch (error) {
+            errorMessage.textContent = 'Error updating profile';
+        }
+    });
+    profile.appendChild(errorMessage);
+    profile.appendChild(successMessage);
+}
+window.addEventListener('DOMContentLoaded', updateEmailFname);
